58b5b3723084772b208b977e4d4ade8c30619904
Angel Crowe <angelcrowe.tech@gmail.com>
2025-05-13 03:14:43 -0700

    revert

---
+++ medical/src/main/java/com/medicalmanager/medical/dto/DoctorAppointmentDto.java
@@ -0,0 +1,52 @@
+package com.medicalmanager.medical.dto;
+
+import java.time.LocalDateTime;
+
+public class DoctorAppointmentDto {
+    private Long id;
+    private String patientName;
+    private String type;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+}
